06c55239de2ec9d5ceea8d7035abf7b9af67a8d1
Richo Febrian <richofal25@gmail.com>
2025-01-08 23:40:46 +0700

    Mengatasi Prop Drilling dengan Component Composition

--- src/App.jsx
+++ src/App.jsx
@@ -80,14 +80,8 @@ function NumResult({ movies }) {
   );
 }
 
-function NavBar({ movies }) {
-  return (
-    <nav className="nav-bar">
-      <Logo />
-      <Search />
-      <NumResult movies={movies} />
-    </nav>
-  );
+function NavBar({ children }) {
+  return <nav className="nav-bar">{children}</nav>;
 }
 
 function MovieList({ movies }) {
@@ -187,21 +181,23 @@ function WatchedList() {
   );
 }
 
-function Main({ movies }) {
-  return (
-    <main className="main">
-      <MovieList movies={movies} />
-      <WatchedList />
-    </main>
-  );
+function Main({ children }) {
+  return <main className="main">{children}</main>;
 }
 
 export default function App() {
   const [movies, setMovies] = useState(tempMovieData);
   return (
     <>
-      <NavBar movies={movies} />
-      <Main movies={movies} />
+      <NavBar>
+        <Logo />
+        <Search />
+        <NumResult movies={movies} />
+      </NavBar>
+      <Main>
+        <MovieList movies={movies} />
+        <WatchedList />
+      </Main>
     </>
   );
 }
